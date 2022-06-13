01e9e344ac2ff99d34b95b515bc49f7d051f9bd4
Khaleeq01 <mohdkhaleeq07@gmail.com>
2022-06-13 23:18:26 +0530

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,75 @@
+//{
+  //type:'ADD_MOVIES';
+  //movies:[m1, m2, m3];
+//}
+//{
+  //  tyoe:'DECREASE_COUNT';
+
+import { data } from "../data";
+
+//}
+//action creator
+export  const ADD_MOVIES='ADD_MOVIES';
+export  const ADD_TO_FAVOURITE='ADD_FAVOURITE';
+export const REMOVE_FROM_FAVOURITE='REMOVE_FROM_FAVOURITE';
+export const SET_SHOW_FAVOURITE='SET_SHOW_FAVOURITE';
+export const ADD_MOVIE_TO_LIST = 'ADD_MOVIE_TO_LIST';
+export const ADD_SEARCH_RESULT='ADD_SEARCH_RESULT';
+//action routes
+export function addMovies(movies){
+  console.log('movies',movies);
+return {
+  type:ADD_MOVIES,
+  movies
+}
+}
+
+export function addFavourite(movie){
+  console.log('movie',movie);
+return {
+  type:ADD_TO_FAVOURITE,
+  movie
+}
+}
+
+export function removeFromFavourite(movie){
+  console.log('movie',movie);
+return {
+  type:REMOVE_FROM_FAVOURITE,
+  movie
+};
+}
+
+export function setShowFavourites(val){
+  console.log('val',val);
+return {
+  type:SET_SHOW_FAVOURITE,
+  val
+};
+}
+ export function addMovieToList(movie){
+ return {
+  type:ADD_MOVIE_TO_LIST,
+  movie
+ };
+ }
+
+export function handleMovieSearch(searchText) {
+  return function (dispatch) {
+    const url = `http://www.omdbapi.com/?apikey=3ca5df7&t=${searchText}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((movie) => {
+        console.log('movie', movie);
+        // dispatch action to save search results in store
+        dispatch(addMovieSearchResult(movie));
+      });
+  };
+}
+
+export function addMovieSearchResult(movie){
+  return {
+    type:ADD_SEARCH_RESULT,
+    movie
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,92 @@
+import { combineReducers } from "redux";
+import { ADD_MOVIES,ADD_TO_FAVOURITE, REMOVE_FROM_FAVOURITE,SET_SHOW_FAVOURITE,ADD_MOVIE_TO_LIST,ADD_SEARCH_RESULT} from "../actions";
+
+const initialMoviesState={
+    list:[],
+    favourites:[],
+    showFavourites:false,
+}
+export  function movies ( state=initialMoviesState, action){
+    console.log('MOVIES REDUCER');
+    // if(action.type===ADD_MOVIES){
+    //     console.log('action',action);
+    //     return {
+    //         ...state,
+    //         list: action.movies
+    //     }
+    // }
+    // return state;
+    switch(action.type){
+        case ADD_MOVIES:
+        return{
+            ...state,
+            list:action.movies
+        }
+        case ADD_TO_FAVOURITE:
+            return{
+                ...state,
+            favourites:[action.movie,...state.favourites]
+            }
+            case REMOVE_FROM_FAVOURITE:
+                const filteredArray = state.favourites.filter(
+            movie=> movie.Title !==action.movie.Title
+                );
+                return {
+            ...state,
+            favourites:filteredArray     
+                };
+            case SET_SHOW_FAVOURITE:
+                return{
+                    ...state,
+                    showFavourites:action.val,
+                }
+                case ADD_MOVIE_TO_LIST:
+                    return {
+                      ...state,
+                      list: [action.movie, ...state.list],
+                    };
+        default:
+            return state;
+    }
+}
+
+const initialSearchState = {
+    results: {},
+    showSearchResults: false,
+  };
+  
+  export function search(state = initialSearchState, action) {
+    switch (action.type) {
+      case ADD_SEARCH_RESULT:
+        return {
+          ...state,
+          results: action.movie,
+          showSearchResults: true,
+        };
+      case ADD_MOVIE_TO_LIST:
+        return {
+          ...state,
+          showSearchResults: false,
+        };
+      default:
+        return state;
+    }
+  }
+  
+  // const initialRootState = {
+  //   movies: initialMoviesState,
+  //   search: []
+  // };
+  
+  // export default function rootReducer(state = initialRootState, action) {
+  //   return {
+  //     movies: movies(state.movies, action),
+  //     search: search(state.search, action)
+  //   };
+  // }
+  
+  export default combineReducers({
+    movies,
+    search,
+  });
+  
